d605f928cd7e87a9fa0d6c6eb27e5c100386b072
Produlx <dulsathlenovo12@gmail.com>
2025-12-11 18:46:20 +0530

    Added height  option

--- BMI.py
+++ BMI.py
@@ -1,7 +1,8 @@
 print("Welcome to BMI Finderr..")
 print("")
 
-W = float(input("Type your Weigth: "))
+W = float(input("Type your Weigth(KG): "))
+H = float(input("Type your height(M): "))
 
 
 '''BMI = float(input())'''
