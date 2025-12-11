e2aac42e857b73d50d02a3171b3f2a1b9e936333
Produlx <dulsathlenovo12@gmail.com>
2025-12-11 19:00:10 +0530

    doing division

--- BMI.py
+++ BMI.py
@@ -4,5 +4,8 @@ print("")
 W = float(input("Type your Weigth(KG): "))
 H = float(input("Type your height(M): "))
 
+BMI  = W/H
+
+'''"print(f"weight {W} / Height {H}"  )"'''
 
 '''BMI = float(input())'''
